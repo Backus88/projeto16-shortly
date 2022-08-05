652b1f2354cdcf7da86c2ef8e768b4191d45b8fc
Luis <lf.mc.almeida@poli.ufrj.br>
2022-08-05 19:58:06 -0300

    sigin with jwt

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,5 +1,10 @@
 import client from '../database/db.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 /**
  * @param  {Object} req the require.
  * @param  {Object} res the request.
@@ -31,13 +36,17 @@ export async function signIn(req, res) {
   const {email, password} = req.body;
   try {
     const thereIsEmail =
-      await client.query(`SELECT password
+      await client.query(`SELECT password, id
                           FROM users 
                           WHERE email = $1`, [email]);
-    const passwordFound = thereIsEmail.rows[0].password;
+    const passwordFound = thereIsEmail.rows[0]?.password;
+    const idFound = thereIsEmail.rows[0]?.id;
+    const data = {userId: idFound};
+    const key = process.env.ACESS_TOKEN;
     const authorization = bcrypt.compareSync(password, passwordFound);
     if (authorization && thereIsEmail.rowCount>0) {
-      res.sendStatus(200);
+      const token =jwt.sign(data, key, {expiresIn: 60*60});
+      res.status(200).send(token);
       return;
     }
     res.sendStatus(401);
