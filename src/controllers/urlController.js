979c00786d9ee39009df15fac47c8f1b8cc90a15
Luis <lf.mc.almeida@poli.ufrj.br>
2022-08-06 03:39:03 -0300

    /urls/open/:shortUrl route

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -15,6 +15,7 @@ export async function shortenUrl(req, res) {
   const shortenUrl = nanoid(8);
   const token = authorization?.replace('Bearer ', '');
   const key = process.env.ACESS_TOKEN;
+  console.log('aki');
   const tokenData = jwt.verify(token, key);
   const urlShort = {shortUrl: shortenUrl};
   const countStart = 0;
@@ -30,6 +31,58 @@ export async function shortenUrl(req, res) {
         VALUES($1,$2,$3)`, [urlId.rows[0].id, tokenData.userId, countStart]);
 
     res.status(201).send(urlShort);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+/**
+ * @param  {Object} req the require.
+ * @param  {Object} res the request.
+ */
+export async function getUrls(req, res) {
+  const urlId = parseInt(req.params.id);
+  try {
+    const thereIsId = await client.query(`SELECT id, "shortUrl", url 
+                                          FROM urls 
+                                          WHERE id = $1`, [urlId]);
+    if (thereIsId.rowCount ===0) {
+      res.sendStatus(404);
+      return;
+    }
+    const urls = {
+      id: thereIsId.rows[0].id,
+      shortUrl: thereIsId.rows[0].shortUrl,
+      url: thereIsId.rows[0].url,
+    };
+
+    res.status(200).send(urls);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+/**
+ * @param  {Object} req the require.
+ * @param  {Object} res the request.
+ */
+export async function getShortUrl(req, res) {
+  const shortUrl = req.params.shortUrl;
+  try {
+    const thereIsUrl = await client.query(`SELECT id, url 
+                                    FROM urls 
+                                    WHERE "shortUrl" = $1`, [shortUrl]);
+
+    console.log(thereIsUrl.rows[0].url);
+    if (thereIsUrl.rowCount===0) {
+      res.sendStatus(404);
+      return;
+    }
+
+    await client.query(`UPDATE likes 
+                        SET "visitCount" = "visitCount" +1 
+                        WHERE "urlId" = $1`, [thereIsUrl.rows[0]?.id]);
+    res.redirect(thereIsUrl.rows[0].url);
   } catch (error) {
     res.status(500).send(error);
   }
--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -1,8 +1,12 @@
 import {Router} from 'express';
-import {shortenUrl} from '../controllers/urlController.js';
+import {shortenUrl,
+  getUrls,
+  getShortUrl} from '../controllers/urlController.js';
 import shortenUrlValidation from '../middlewares/shortenurlValidation.js';
 
 const urlRouter = Router();
 urlRouter.post('/urls/shorten', shortenUrlValidation, shortenUrl);
+urlRouter.get('/urls/:id', getUrls);
+urlRouter.get('/urls/open/:shortUrl', getShortUrl);
 
 export default urlRouter;
